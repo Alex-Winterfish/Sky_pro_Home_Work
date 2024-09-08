a4eeadf09b678cfff973a1cc5a9297a8d09a6364
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-09-08 17:07:15 +0300

    в модуле processing написана функция sort_by_date, которая сортирует список словарей по дате. написан файл readme.md. кот оформлен согласно pep8

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,24 @@
-def filter_by_state(a: list, state = 'EXECUTED' )-> list:
-    """Функция принимает список словарей и параметр. Возвращает список словарей с ключами, соответствующими параметру"""
-    b=list()
-    for i in range(len(a)): # цикл для поиска элемента словаря с ключом, соответствующему параметру
-        if a[i].get('state') == state:
-            b.append(a[i])
-    return(b)
-
 from datetime import datetime
-def sort_by_date(a:list, sort_option = True)->list:
-    #Функция для сортировки списка словаряей по дате, по умолчанию список сортируется по убыванию
-    return sorted(a, key=lambda a: datetime.strptime(a['date'][0:10], "%Y-%m-%d").date(), reverse = sort_option)
+
+
+def filter_by_state(input_list: list, state="EXECUTED") -> list:
+    """Функция принимает список словарей и параметр.
+    Возвращает список словарей с ключами, соответствующими параметру"""
+    b = list()
+    for i in range(
+        len(input_list)
+    ):  # цикл для поиска элемента словаря с ключом, соответствующему параметру
+        if input_list[i].get("state") == state:
+            b.append(input_list[i])
+    return b
+
+
+def sort_by_date(input_list: list, sort_option=True) -> list:
+    """Функция для сортировки списка словарей по дате, по умолчанию список сортируется по убыванию"""
+    return sorted(
+        input_list,
+        key=lambda input_list: datetime.strptime(
+            input_list["date"][0:10], "%Y-%m-%d"
+        ).date(),
+        reverse=sort_option,
+    )
