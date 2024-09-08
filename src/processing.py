3a43f4fed1ce103fba883f6a39e83029a41b307c
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-09-08 15:16:19 +0300

    в модуле processing.py написана функцию sort_by_date, которая сортирует список словарей по дате

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,12 @@
-def filter_by_state(a: list, state = "EXECUTED" )-> list:
+def filter_by_state(a: list, state = 'EXECUTED' )-> list:
+    """Функция принимает список словарей и параметр. Возвращает список словарей с ключами, соответствующими параметру"""
     b=list()
-    for i in range(len(a)):
+    for i in range(len(a)): # цикл для поиска элемента словаря с ключом, соответствующему параметру
         if a[i].get('state') == state:
             b.append(a[i])
     return(b)
+
+from datetime import datetime
+def sort_by_date(a:list, sort_option = True)->list:
+    #Функция для сортировки списка словаряей по дате, по умолчанию список сортируется по убыванию
+    return sorted(a, key=lambda a: datetime.strptime(a['date'][0:10], "%Y-%m-%d").date(), reverse = sort_option)
