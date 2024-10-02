79bb3d0c16245a96d3b0d5c75d022f1d052aad04
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-10-02 17:56:02 +0300

    написаны логи для модуля utils и masks

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,9 @@
 import logging
+
 logger = logging.getLogger("masks")
-handler = logging.FileHandler('../logs/masks.log', 'w')
+handler = logging.FileHandler("../logs/masks.log", "w")
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -10,7 +11,9 @@ logger.addHandler(handler)
 def get_mask_card_number(card_num: int) -> str:
     """Функция, которая принимает номер карты и возвращает маску номера"""
     if len(str(card_num)) > 10:
-        logger.info("введен номер карты достаточной длинны для наложения маски, накладываем маску")
+        logger.info(
+            "введен номер карты достаточной длинны для наложения маски, накладываем маску"
+        )
         card_num_mask_delimited = ""
         card_num_mask = (
             str(card_num)[0:6] + "*" * (len(str(card_num)) - 10) + str(card_num)[-4:]
@@ -29,15 +32,20 @@ def get_mask_card_number(card_num: int) -> str:
         logger.error("номер карты слишком короткий")
         return "номер карты слишком короткий для наложения маски"
 
-card_mask = get_mask_card_number(87685464564564546)
+
+# card_mask = get_mask_card_number(87685464564564546) #раскомитить для проверки лигирования
 
 
 def get_mask_account(account: int) -> str:
     """Функция, которая принимает номер счета и возвращает маску номера"""
     if len(str(account)) > 4:
-        logger.info("введен номер  счета достаточной длинны для наложения маски, накладываем маску")
+        logger.info(
+            "введен номер  счета достаточной длинны для наложения маски, накладываем маску"
+        )
         return "**" + str(account)[-4:]
     else:
         logger.error("номер счета слишком короткий для наложения маски")
         return "номер счета слишком короткий для наложения маски"
-account_mask = get_mask_account(23443232)
+
+
+# account_mask = get_mask_account(23443232) #раскомитить для проверки лигирования
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import logging
+
 logger = logging.getLogger("utils")
-handler = logging.FileHandler('../logs/utils.log', 'w')
+handler = logging.FileHandler("../logs/utils.log", "w")
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-file_path = "../data/operations.json"
+# file_path = "../data/operations.json" #раскомитить для проверки лигирования #раскомитить для проверки лигирования
+
 
 def get_transaction_data(file_path: str) -> list:
     "функция принимает путь до json файла и возвращает список словарей"
@@ -20,49 +22,55 @@ def get_transaction_data(file_path: str) -> list:
         with open(os.path.abspath(file_path), "r", encoding="utf-8") as f:
             # обрабатываем ошибку декодирования файла
             try:
-                logger.info(f'Получаем данные из файла {file_path}')
+                logger.info(f"Получаем данные из файла {file_path}")
                 data = json.load(f)
             except json.JSONDecodeError:
-                logger.error(f'Ошибка декодирования файла {file_path}')
+                logger.error(f"Ошибка декодирования файла {file_path}")
                 print("Ошибка декодирования файла")
                 return output_list
     except FileNotFoundError:
-        logger.error(f'Файл {file_path} не найден')
+        logger.error(f"Файл {file_path} не найден")
         print("Файл не найден")
         return output_list
     return data
 
-transaction_data = get_transaction_data(file_path)
+
+# transaction_data = get_transaction_data(file_path)
 def transaction_sum(transaction: dict) -> float:
     "Функция возвращает сумму транзакций"
     from src.external_api import amount_exchange
 
     try:  # обрабатываем ошибку функции конвертации валюты amount_exchange
 
-        logger.info('получаем словарь с транзакцией')
+        logger.info("получаем словарь с транзакцией")
         if transaction != {}:
             if transaction.get("operationAmount").get("currency").get("code") == "RUB":
-                logger.info('Транзакция совершена в рублях, получаем сумму транзакции')
+                logger.info("Транзакция совершена в рублях, получаем сумму транзакции")
                 output_sum = float(transaction.get("operationAmount").get("amount"))
             elif (
                 transaction.get("operationAmount").get("currency").get("code") == "USD"
             ):
-                logger.info('Транзакция совершена в валюте USD, получаем сумму транзакции')
+                logger.info(
+                    "Транзакция совершена в валюте USD, получаем сумму транзакции"
+                )
                 usd_sum = float(transaction.get("operationAmount").get("amount"))
                 output_sum = amount_exchange(usd_sum, "USD", "RUB")
             elif (
                 transaction.get("operationAmount").get("currency").get("code") == "EUR"
             ):
-                logger.info('Транзакция совершена в валюте EUR, получаем сумму транзакции')
+                logger.info(
+                    "Транзакция совершена в валюте EUR, получаем сумму транзакции"
+                )
                 eur_sum = float(transaction.get("operationAmount").get("amount"))
                 output_sum = amount_exchange(eur_sum, "EUR", "RUB")
         else:
-            logger.warning('Данные по транзакции не найдены')
+            logger.warning("Данные по транзакции не найдены")
             return dict()
 
     except TypeError:
-        logger.error('Ошибка конвертации валюты')
+        logger.error("Ошибка конвертации валюты")
         return "Ошибка конвертации валюты"
     return output_sum
 
-transaction_sum(transaction_data[0])
+
+# transaction_sum(transaction_data[0]) #раскомитить для проверки лигирования
