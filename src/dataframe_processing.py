fe9482cbe9bed5899bb632cccc364764c24e949d
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-10-06 09:13:56 +0300

    написан модуль dataframe_processing  с функциями csv_processing и excel_processing, которые выводят списки словарей с транзакциями из файлов csv и excel

--- src/dataframe_processing.py
+++ src/dataframe_processing.py
@@ -1,28 +1,49 @@
-import csv
-
 filepath = "../data/transactions.csv"
-def csv_processing(filepath:str)->list:
+
+
+def csv_processing(filepath: str) -> list:
+    """Функция принимает путь к файлу csv и возвращает список словарей с транзакциями"""
+    import csv
+
     output_list = list()
+    try:
+        with open(filepath, encoding="utf-8") as csv_file:
 
-    with open(filepath, encoding="utf-8") as csv_file:
+            csv_reader = csv.DictReader(
+                csv_file, delimiter=";"
+            )  # читаем файл как словарь
 
-        csv_reader = csv.DictReader(csv_file, delimiter=';')
+            for row in csv_reader:  # цикл для добавления словарей в список
+                output_list.append(row)
+                next(csv_reader)
 
-        for row in csv_reader:
-            output_list.append(row)
-            next(csv_reader)
-
-    return output_list
+        return output_list
+    except FileNotFoundError:
+        return f"Файл по адресу {filepath} не найден"
 
 
-#print(csv_processing(filepath))
-#print(f'число элемнтов списка {len(csv_processing(filepath))}')
+filepath_excel = "../data/transactions_excel.xlsx"
 
-filepath_excel = '../data/transactions_excel.xlsx'
-def excel_processing(filepath:str):
+
+def excel_processing(filepath: str) -> list:
+    """Функция принимает путь к файлу xls и возвращает список словарей с транзакциями"""
     import pandas as pd
-    transaction_data_xls = pd.read_excel(filepath)
-    x = "id"
-    return transaction_data_xls[x][0]
 
-print(excel_processing(filepath_excel))
+    try:
+        transaction_data_xls = pd.read_excel(filepath).to_dict(
+            "index"
+        )  # читаем файл excel и переводим в словарь
+        output_list = list()
+        for (
+            values
+        ) in (
+            transaction_data_xls.values()
+        ):  # цикл для добавления словарй с транзакциями в список
+            output_list.append(values)
+
+        return output_list
+    except FileNotFoundError:
+        return f"Файл по адресу {filepath_excel} не найден"
+
+
+print(csv_processing(filepath))
