96bea43e4c4e1b58eced03562d0f1ee32c863a5b
Алексей Булгаков <Alksbulgakov@gmail.com>
2024-10-09 21:27:55 +0300

    начата работа над основным пакетом с логикой прогаммы main.py

---
+++ src/main.py
@@ -0,0 +1,49 @@
+
+file_path_json = "../data/operations.json"
+file_path_csv = "../data/transactions.csv"
+file_path_excel = "../data/transactions_excel.xlsx"
+def main():
+    from src.utils import get_transaction_data
+    from src.dataframe_processing import csv_processing, excel_processing
+    from src.processing import filter_by_state
+    user_input = input(
+        " Привет! Добро пожаловать в программу работы с банковскими транзакциями.\n"
+        " Выберите необходимый пункт меню:\n "
+        "1. Полоучить информацию о транзакциях из JSON-файла\n "
+        "2. Полоучить информацию о транзакциях из CSV-файла\n "
+        "3. Полоучить информацию о транзакциях из XLSX-файла\n "
+        "ввод: "
+    )
+
+    if user_input =="1": # получаем список транзакций из json файла
+        print("Для обработки выбран json-файл")
+        transaction_list = get_transaction_data(file_path_json)
+
+    elif user_input == "2": # получаем список транзакций из csv файла
+        print("Для обработки выбран csv-файл")
+        transaction_list = csv_processing(file_path_csv)
+
+    elif user_input == "3":# получаем список транзакций из excel файла
+        print("Для обработки выбран excel-файл")
+        transaction_list = excel_processing(file_path_excel)
+    while True:
+        state = input("Введите статус, по которому необходимо выполнить фильтрацию.\nДоступные для фильтрации статусы:\n"
+                  "EXECUTED, CANCELED, PENDING\n"
+                  "ввод: ").upper()
+        if state == "EXECUTED" or "CANCELED" or "PENDING":
+            break
+            print(f'Операции отфильтрованы по статусу {state}')
+        else:
+            print(f"Статус операции {state} недоступен")
+    output_data = filter_by_state(transaction_list, state=state)
+
+
+    return output_data
+
+
+
+
+if __name__ == "__main__":
+
+    file_path = "../data/operations.json"
+    print(main())
